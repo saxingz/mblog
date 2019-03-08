edfca7123bea69401a1a7b528d9a8131f5a610df
saxing <saxlh@foxmail.com>
2019-03-08 14:17:28 +0800

    更新

--- src/main/resources/static/dist/js/modules/markdown.js
+++ src/main/resources/static/dist/js/modules/markdown.js
@@ -11,26 +11,11 @@
 define('markdown', [
     'codemirror-css',
     'codemirror-theme',
-    'codemirror'
+    'codemirror',
+    'marked'
     ], function(require, exports, module) {
 
-    require.async(['codemirror-keymap'], function () {
-        var editor = CodeMirror.fromTextArea(document.getElementById("content"), {
-            mode: 'markdown',     // Markdown
-            lineNumbers: true,     // 显示行数
-            indentUnit: 4,         // 缩进4格
-            tabSize: 4,
-            autoCloseBrackets: true,
-            matchBrackets: true,   // 括号匹配
-            lineWrapping: true,    // 自动换行
-            theme: 'idea',
-            keyMap: 'sublime',
-            extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
-        });
-        editor.setSize('auto', '450px');
-
-        editor.on('change', function (cm, co) {
-            $('#content').text(cm.getValue());
-        });
+    require.async(['codemirror-markdown', 'codemirror-keymap', 'app.markdown'], function () {
+        MkEditor.initEditor();
     });
 });
